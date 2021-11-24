2a90967d367cd75b49b9a7a5ecc81205a9162cf7
ysuncpacanada <70352043+ysuncpacanada@users.noreply.github.com>
2021-11-23 17:45:49 -0800

    fourth commit = from master branch

--- VSGitDemoProject/Program.cs
+++ VSGitDemoProject/Program.cs
@@ -9,6 +9,7 @@ namespace VSGitDemoProject
             int a = 1;
             int b = 2;
             int c = 3;
+            int d = 4
             Console.WriteLine("Hello World!");
         }
     }
